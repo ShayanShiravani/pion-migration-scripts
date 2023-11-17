0bf2232f95baef6647af7af6b4f39dbf7d42aa64
Shayan Shiravani <shayan5420@yahoo.com>
2023-11-18 02:44:21 +0330

    rewardManager script

---
+++ importRewards.ts
@@ -0,0 +1,68 @@
+import Web3, { Web3BaseWalletAccount } from 'web3';
+import {abi} from './abis/MuonRewardManager.json';
+import {promises as fs} from 'fs';
+import { parse } from 'csv-parse/sync';
+import 'dotenv/config';
+
+const RPC_URL = "https://rpc.ankr.com/bsc_testnet_chapel"
+const MAX_GAS = "7000000"
+const FILE_NAME = "./data/rewards.csv"
+
+const missingPrivateKey = () => {
+  throw Error('PrivateKey missing')
+}
+
+const missingContractAddress = () => {
+  throw Error('MuonRewardManager address missing')
+}
+
+const main = async () => {
+  const contractAddr: string = process.env.MUON_REWARD_MANAGER || missingContractAddress()
+  const privateKey: string = process.env.PRIVATE_KEY || missingPrivateKey()
+
+  const web3 = new Web3(RPC_URL)
+  const account: Web3BaseWalletAccount = web3.eth.accounts.privateKeyToAccount(
+    `0x${privateKey}`
+  )
+
+  const args: Array<string> = process.argv.slice(2)
+  const content = await fs.readFile(`./${FILE_NAME}`)
+  const records = await parse(content, {
+      bom: true,
+      delimiter: ",", 
+      from_line: parseInt(args[0]),
+      to_line: parseInt(args[1]) 
+  })
+
+  let users: Array<string> = []
+  let rewardAmounts: Array<string> = []
+  let tokenIds: Array<string> = []
+
+  records.map((row: Array<string>) => {
+    users.push(row[0])
+    rewardAmounts.push(row[1])
+    tokenIds.push(row[2])
+  })
+
+
+  const contract = new web3.eth.Contract(abi, contractAddr)
+  const tx = (contract.methods.migrate as any)(
+    users, rewardAmounts, tokenIds
+  )
+
+  const options = {
+    to: contractAddr,
+    data: tx.encodeABI(),
+    gas: MAX_GAS,
+    gasPrice: await web3.eth.getGasPrice(),
+    nonce: await web3.eth.getTransactionCount(account.address)
+  }
+  const signed  = await web3.eth.accounts.signTransaction(options, privateKey)
+  const receipt = await web3.eth.sendSignedTransaction(signed.rawTransaction)
+
+  console.log(receipt.transactionHash)
+}
+
+main()
+
+
