eb80f945985a09408715f2ec1d72c71cfef473c9
Shayan Shiravani <shayan6959@gmail.com>
2025-04-06 11:48:47 +0330

    import bonMUON

--- importBonMUONs.js
+++ importBonMUONs.js
@@ -1,31 +1,31 @@
-import Web3, { Web3BaseWalletAccount } from 'web3';
-import {abi} from './abis/BondedPION.json';
+import Web3 from 'web3';
+import ABI from './abis/BondedMUON.json' assert { type: "json" };
 import {promises as fs} from 'fs';
 import { parse } from 'csv-parse/sync';
 import 'dotenv/config';
 
-const RPC_URL = "https://rpc.ankr.com/bsc_testnet_chapel"
+const RPC_URL = "https://avalanche-fuji-c-chain-rpc.publicnode.com"
 const MAX_GAS = "7000000"
-const FILE_NAME = "./data/bonPIONs.csv"
+const FILE_NAME = "./data/bonMUONs.csv"
 
 const missingPrivateKey = () => {
   throw Error('PrivateKey missing')
 }
 
 const missingContractAddress = () => {
-  throw Error('BondedPION address missing')
+  throw Error('BondedMUON address missing')
 }
 
 const main = async () => {
-  const contractAddr: string = process.env.BONDED_PION_ADDRESS || missingContractAddress()
-  const privateKey: string = process.env.PRIVATE_KEY || missingPrivateKey()
+  const contractAddr = process.env.BONDED_MUON_ADDRESS || missingContractAddress()
+  const privateKey = process.env.PRIVATE_KEY || missingPrivateKey()
 
   const web3 = new Web3(RPC_URL)
-  const account: Web3BaseWalletAccount = web3.eth.accounts.privateKeyToAccount(
+  const account = web3.eth.accounts.privateKeyToAccount(
     `0x${privateKey}`
   )
 
-  const args: Array<string> = process.argv.slice(2)
+  const args = process.argv.slice(2)
   const content = await fs.readFile(`./${FILE_NAME}`)
   const records = await parse(content, {
       bom: true,
@@ -34,23 +34,23 @@ const main = async () => {
       to_line: parseInt(args[1]) 
   })
 
-  let tokenIds: Array<string> = []
-  let owners: Array<string> = []
-  let balances: Array<string> = []
-  let mintedAts: Array<string> = []
+  let tokenIds = []
+  let owners = []
+  let balances = []
+  let mintedAts = []
 
-  const now = Date.now()/1000
+  const now = Math.floor(Date.now()/1000)
 
-  records.map((row: Array<string>, i: number) => {
-    tokenIds.push(`${++i}`)
+  records.map((row, i) => {
+    tokenIds.push(row[2])
     owners.push(row[0])
     balances.push(row[1])
     mintedAts.push(now.toString())
-  })
+  });
 
 
-  const contract = new web3.eth.Contract(abi, contractAddr)
-  const tx = (contract.methods.migrate as any)(
+  const contract = new web3.eth.Contract(ABI.abi, contractAddr)
+  const tx = contract.methods.migrate(
     tokenIds, owners, balances, mintedAts
   )
 
