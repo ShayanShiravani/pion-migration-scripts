9a3444e0fafb372c42cc537854b3a76a3b3c1d8b
Shayan Shiravani <shayan6959@gmail.com>
2025-06-24 09:31:16 +0330

    new migration data

--- importStakes.js
+++ importStakes.js
@@ -6,7 +6,7 @@ import 'dotenv/config';
 
 const RPC_URL = "https://endpoints.omniatech.io/v1/avax/mainnet/public"
 const MAX_GAS = "7000000"
-const FILE_NAME = "./data/stakes-v6.csv"
+const FILE_NAME = "./data/stakes-v7.csv"
 
 const missingPrivateKey = () => {
   throw Error('PrivateKey missing')
